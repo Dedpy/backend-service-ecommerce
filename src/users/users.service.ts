b2d9bf9d2cb3e220b3618719b3b80c39697680c0
Dedpy <wassimbenr@gmail.com>
2023-11-03 20:47:36 +0100

    feat: implementing MySql database

---
+++ src/users/users.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from 'src/models/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    return this.userRepository.save(createUserDto);
+  }
+
+  async findAll(): Promise<User[]> {
+    try {
+      return await this.userRepository.find({});
+    } catch (err) {
+      return err;
+    }
+  }
+
+  async findOne(id: number): Promise<User> {
+    try {
+      return await this.userRepository.findOneBy({ id });
+    } catch (err) {
+      return err;
+    }
+  }
+
+  async update(
+    id: number,
+    updateUserDto: UpdateUserDto,
+  ): Promise<User | undefined> {
+    const user = await this.userRepository.findOneBy({ id });
+    if (!user) {
+      return undefined;
+    }
+    this.userRepository.merge(user, updateUserDto);
+    return this.userRepository.save(user);
+  }
+  async remove(id: number): Promise<void> {
+    await this.userRepository.delete(id);
+  }
+}
